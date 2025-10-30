69fc744f86c8a5e8039ae7a0b4ca547f3d4ae56b
Ansh-Patoliya <anshpatoliya1408@gmail.com>
2025-10-30 06:34:23 +0530

    Add implementation for Permutation in String problem with sliding window approach

--- src/Array/SlidingWindow/PermutationInString.java
+++ src/Array/SlidingWindow/PermutationInString.java
@@ -1,5 +1,7 @@
 package Array.SlidingWindow;
 
+import java.util.HashMap;
+
 /**
  * Problem: Permutation in String (LeetCode 567)
  *
@@ -30,4 +32,36 @@ package Array.SlidingWindow;
  */
 // Leetcode 567. Permutation in String
 public class PermutationInString {
+    boolean per(String s1,String s2){
+        HashMap<Character,Integer> target=new HashMap<>();
+        HashMap<Character,Integer> window=new HashMap<>();
+
+        if(s1.length()>s2.length())
+            return false;
+        for (int i = 0; i < s1.length(); i++) {
+            target.put(s1.charAt(i),target.getOrDefault(s1.charAt(i),0)+1);
+            window.put(s2.charAt(i),window.getOrDefault(s2.charAt(i),0)+1);
+        }
+        if(target.equals(window))
+            return true;
+        for (int i = s1.length(); i <s2.length() ; i++) {
+            window.put(s2.charAt(i),window.getOrDefault(s2.charAt(i),0)+1);
+            int a= window.get(s2.charAt(i-s1.length()));
+            if(a==1)
+                window.remove(s2.charAt(i-s1.length()));
+            else
+                window.replace(s2.charAt(i-s1.length()),--a);
+
+            if(target.equals(window))
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PermutationInString permutationInString=new PermutationInString();
+        String s1="ab";
+        String s2="eidbaooo";
+        System.out.println(permutationInString.per(s1,s2));
+    }
 }
