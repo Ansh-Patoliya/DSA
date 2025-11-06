4a35ec41a3870f6fb3e6846df7d0626327180067
Ansh-Patoliya <anshpatoliya1408@gmail.com>
2025-11-06 17:26:19 +0530

    Implement getIntersectionNode method to find the intersection of two linked lists

--- src/LinkedList/AdvancedProblems/IntersectionOfTwoLinkedLists.java
+++ src/LinkedList/AdvancedProblems/IntersectionOfTwoLinkedLists.java
@@ -84,4 +84,20 @@ public class IntersectionOfTwoLinkedLists {
         }
         System.out.println("null");
     }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA=headA;
+        ListNode pB=headB;
+        while(pA!=pB){
+            if (pA==null)
+                pA=headB;
+            else
+                pA=pA.next;
+
+            if (pB==null)
+                pB=headA;
+            else
+                pB=pB.next;
+        }
+        return pA;
+    }
 }
